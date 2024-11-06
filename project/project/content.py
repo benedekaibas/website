26003fe59e295fd57fad5cae198907934173412c
benedekaibas <kaibas.benedek02@gmail.com>
2024-11-06 14:46:28 -0500

    progress with website structure

--- project/project/content.py
+++ project/project/content.py
@@ -8,44 +8,87 @@ class Content():
 
     def generate_index_content(self):
         return """
-        ---
-        title: "Home"
-        ---
+---
+title: "Home"
+---
 
-        # Welcome to My Website
+# Welcome to My Website
 
-        ## About Me
-        I am a passionate developer with interests in various fields of computer science. 
-        [Read more](about.qmd)
+## About Me
+I am a passionate developer with interests in various fields of computer science. 
+[Read more](about.qmd)
 
-        ## Computer Science
-        I have worked on numerous projects involving algorithms, data structures, and software engineering principles.
-        [Read more](computer-science.qmd)
+## Computer Science
+I have worked on numerous projects involving algorithms, data structures, and software engineering principles.
+[Read more](computer-science.qmd)
 
-        ## My Hungarian Thoughts
-        I often share my thoughts and experiences related to Hungarian culture and language.
-        [Read more](hungarian-thoughts.qmd)
+## My Hungarian Thoughts
+I often share my thoughts and experiences related to Hungarian culture and language.
+[Read more](hungarian-thoughts.qmd)
         """
 
     def generate_about_content(self):
         return """
-        # About Me
+---
+title: "Finley Malloc"
+about:
+  id: hero-heading
+  template: jolla
+  image: profile.jpg
+  links:
+    - icon: twitter
+      text: twitter
+      href: https://twitter.com
+    - icon: github
+      text: Github
+      href: https://github.com
+---
 
-        This is the About Me section.
+### This content appears above the formatted about page content.
+
+:::{#hero-heading}
+
+Finley Malloc is the Chief Data Scientist at Wengo Analytics. When not innovating on data platforms, Finley enjoys spending time unicycling and playing with her pet iguana.
+
+## Education
+
+University of California, San Diego | San Diego, CA
+PhD in Mathematics | Sept 2011 - June 2015
+
+Macalester College | St. Paul MA
+B.A in Economics | Sept 2007 - June 2011
+
+## Experience
+
+Wengo Analytics | Head Data Scientist | April 2018 - present
+
+GeoScynce | Chief Analyst | Sept 2012 - April 2018
+
+:::
+
+### This content appears below the formatted about page content.
         """
 
     def generate_computer_science_content(self):
         return """
-        # Computer Science
+---
+title: "Computer Science"
+---
 
-        This is the Computer Science section.
+# Computer Science
+
+This is the Computer Science section.
         """
 
     def generate_hungarian_thoughts_content(self):
         return """
-        # My Hungarian Thoughts
+---
+title: "My Hungarian Thoughts"
+---
 
-        This is the My Hungarian Thoughts section.
+# My Hungarian Thoughts
+
+This is the My Hungarian Thoughts section.
         """
     
 if __name__ == "__main__":
